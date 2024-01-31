d38572575c9ab899913a322c5b485e5e1425d0bf
Rafael <rafael.c.pinheiro96@gmail.com>
2024-01-31 18:58:26 -0300

    ajustar periodo do indice

--- app.py
+++ app.py
@@ -41,6 +41,9 @@ st.markdown("Após os tratamentos, ficamos com a seguinte série temporal:")
 df_timeseries = pd.read_csv(
     create_driver_path(file_id=constants.TIMESERIES_FILE_ID.value)
 ).set_index("data", drop=True)
+df_timeseries["data"] = pd.to_datetime(df_timeseries["data"]).to_period("D")
+df_timeseries = df_timeseries.set_index("data", drop=True)
+
 st.write(df_timeseries.head(20))
 
 st.markdown("## Análise da série temporal")
