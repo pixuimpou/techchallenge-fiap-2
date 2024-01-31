9ba5e57d1735d260a303c672e8c6ed720c9b8778
Rafael <rafael.c.pinheiro96@gmail.com>
2024-01-31 18:26:54 -0300

    ajuste timeseries

--- app.py
+++ app.py
@@ -38,5 +38,5 @@ st.markdown(
 st.markdown("Após os tratamentos, ficamos com a seguinte série temporal:")
 df_timeseries = pd.read_csv(
     create_driver_path(file_id=constants.TIMESERIES_FILE_ID.value)
-)
+).set_index("data", drop=True)
 st.write(df_timeseries.head(20))
